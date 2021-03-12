508a4ce9111f9baabd06aa44da673b7fd4e86b70
mkolodziejczyk-piap <73108914+mkolodziejczyk-piap@users.noreply.github.com>
2021-03-12 14:59:41 +0100

    Update mastr1325.py

--- references/segmentation/mastr1325.py
+++ references/segmentation/mastr1325.py
@@ -1,1 +1,61 @@
+import os
+import shutil
+from torchvision.datasets import VisionDataset
+from typing import Any, Callable, Optional, Tuple
 
+import numpy as np
+
+from PIL import Image
+
+
+class MaSTr1325Dataset(VisionDataset):
+    """
+    """
+
+    # url = "http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz"
+
+    def __init__(
+            self,
+            root: str,
+            image_set: str = "train",
+            download: bool = False,
+            transforms: Optional[Callable] = None,
+    ) -> None:
+
+        super(MaSTr1325Dataset, self).__init__(root, transforms)
+
+        Top_Img_Path = '/content/MaSTr1325_images'
+        Top_Mask_Path = '/content/MaSTr1325_masks'
+        
+        self.images = sorted(
+            [
+                os.path.join(Top_Img_Path, fname)
+                for fname in os.listdir(Top_Img_Path)
+                if fname.endswith(".jpg")
+            ]
+        )
+        self.masks = sorted(
+            [
+                os.path.join(Top_Mask_Path , fname)
+                for fname in os.listdir(Top_Mask_Path )
+                if fname.endswith(".png") and not fname.startswith(".")
+            ]
+        )
+
+        assert (len(self.images) == len(self.masks))
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        img = Image.open(self.images[index]).convert('RGB')
+        target = Image.open(self.masks[index])
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def extra_repr(self) -> str:
+        lines = ["Image set: {image_set}", "Mode: {mode}"]
+        return '\n'.join(lines).format(**self.__dict__)
